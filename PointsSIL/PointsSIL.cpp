b5871c36a1a2de47d4840288f33ee1256affe287
PC <fahler114@gmail.com>
2021-04-21 21:14:37 -0300

    Agregar archivos de proyecto.

---
+++ PointsSIL/PointsSIL.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+int main()
+{
+
+	struct Point
+	{
+		int x;
+		int y;
+	};
+
+
+
+
+
+
+	std::vector<Point> puntos;
+
+	//Z=A(X)+B(Y);
+	int a;
+	std::cout << "insert A"<<std::endl;
+	std::cin >> a;
+
+	int b;
+	std::cout << "insert B" << std::endl;
+	std::cin >> b;
+
+	int cant;
+	std::cout << "Point Count :" << std::endl;
+	std::cin >> cant;
+
+	for (int i = 0; i < cant; ++i)
+	{
+		Point punto;
+		std::cout << "insert point " <<(i+1)<<" :"<< std::endl;
+		std::cin >> punto.x >> punto.y;
+
+		puntos.push_back(punto);
+
+	}
+	for (int i = 0; i < cant; ++i)
+	{
+		int res = (a * puntos[i].x) + (b * puntos[i].y);
+		std::cout << "en el punto " << (i + 1) << " Z es: " << res << std::endl;
+
+	}
+
+
+
+
+
+
+
+   
+}
